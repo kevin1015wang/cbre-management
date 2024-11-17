c5375a98c20356cae54fc653f100032705785ef6
Taylor-Klinsky <65639159+Taylor-Klinsky@users.noreply.github.com>
2024-11-17 04:07:21 +0000

    modified:   cbre-management-frontend/package-lock.json 	modified:   cbre-management-frontend/package.json 	modified:   cbre-management-frontend/src/pages/metrics.js 	new file:   package-lock.json commit

--- cbre-management-frontend/src/pages/metrics.js
+++ cbre-management-frontend/src/pages/metrics.js
@@ -1,23 +1,44 @@
+import { Card } from "baseui/card";
 import React from "react";
-import { PieChart } from "react-google-charts";
+import { Chart } from "react-google-charts";
 
-
-export default function MetricsPage() {
+function pieChartCard() {
   return (
-    <div>Metrics Page</div>
+    <>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
+    </>
   );
 }
 
-<PieChart
-  series={[
-    {
-      data: [
-        { id: 0, value: 10, label: 'series A' },
-        { id: 1, value: 15, label: 'series B' },
-        { id: 2, value: 20, label: 'series C' },
-      ],
-    },
-  ]}
-  width={400}
-  height={200}
-/>
+export default function MetricsPage() {
+  return (
+    <>
+      <div>Metrics Page</div>
+      {pieChartCard()}
+    </>
+  );
+}
+
+const options = {
+  title: "Building 1 Electricity Usage",
+};
+
+const data = [
+  ["Source", "Cost"],
+  ["Air conditioning", 9],
+  ["Refrigeration", 2],
+  ["Ventilation", 2],
+  ["Computers", 2],
+  ["Lights", 7],
+];
+
+const config = {
+  type: 'doughnut',
+  data: data,
+};
