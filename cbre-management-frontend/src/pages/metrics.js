46dc5ca19ddd9de3e77755aabb453c3d4be671e4
Taylor-Klinsky <65639159+Taylor-Klinsky@users.noreply.github.com>
2024-11-17 06:14:30 +0000

    commit

--- cbre-management-frontend/src/pages/metrics.js
+++ cbre-management-frontend/src/pages/metrics.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Chart } from "react-google-charts";
 import { LightTheme, BaseProvider, styled } from "baseui";
+import { Card } from "baseui/card";
 import { Bar } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -12,6 +13,8 @@ import {
   Tooltip,
   Legend
 } from 'chart.js';
+import PieChartCard from "../components/pieChartCard";
+import BarChartCard from "../components/barChartCard";
 
 ChartJS.register(
   Title,
@@ -23,87 +26,26 @@ ChartJS.register(
   BarController
 );
 
-const ContentWrapper = styled("div", {
+const Row = styled("div", {
   display: "flex",
   flexDirection: "row",
   gap: "16px",
-  padding: "16px",
-  overflow: "hidden",
+  overflowY: "auto",
 });
-
-function pieChartCard() {
-  return (
-    <ContentWrapper style={{ maxWidth: "1300px", margin: "0 auto"}}>
-      <Chart
-        chartType="PieChart"
-        data={data}
-        options={options}
-        width={"100%"}
-        height={"400px"}
-      />
-      <div>Helpful Insights</div>
-    </ContentWrapper>
-  );
-}
-
-const BarChart = () => {
-  const data = {
-    labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'],
-    datasets: [
-      {
-        label: 'Data Series 1',
-        backgroundColor: 'rgba(75, 192, 192, 0.6)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1,
-        data: [12, 19, 3, 5, 2],
-      },
-    ],
-  };
-
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-
-  return (
-    <div>
-      <Bar data={data} options={options} />
-    </div>
-  );
-};
-
-function barChartCard() {
-  return (
-    <ContentWrapper style={{ maxWidth: "1300px", margin: "0 auto"}}>
-      <BarChart/>
-      <div>Helpful Insights</div>
-    </ContentWrapper>
-  );
-}
-
-const options = {
-  title: "Building 1 Electricity Usage",
-};
-
-const data = [
-  ["Source", "Cost"],
-  ["Air conditioning", 9],
-  ["Refrigeration", 2],
-  ["Ventilation", 2],
-  ["Computers", 2],
-  ["Lights", 7],
-];
 
 export default function MetricsPage() {
   return (
     <>
       <div>Metrics Page</div>
-      {pieChartCard()}
-      {pieChartCard()}
-      {barChartCard()}
+      <Row>
+        {PieChartCard()}
+        {PieChartCard()}
+        {BarChartCard()}
+      </Row>  
+      <Row>
+        {BarChartCard()}
+        {BarChartCard()}
+      </Row>    
     </>
   );
 }
